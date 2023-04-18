9029a14990fff3231b0e662fd178f7bfe83138dd
OMEN <j.manuelvaldebenito@gmail.com>
2023-04-18 00:28:33 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useRef } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <p className='text-9xl'>Hola</p>
+    </div>
+  )
+}
+
+export default App
