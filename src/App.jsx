69016a2349c45b271a8a9d300c52b80876fdfa41
OMEN <j.manuelvaldebenito@gmail.com>
2023-04-21 02:23:55 -0400

    ADD: Input added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import { useRef } from 'react'
+import { useState, useEffect } from 'react'
+// import {data} from './assets/Libro1.csv'
+import Papa from 'papaparse'
 import './App.css'
+import {Bar} from 'react-chartjs-2'
+import { CategoryScale, LinearScale,
+  BarElement, Title, Tooltip, Legend } from 'chart.js'
+
+// ChartJS.register(
+//   CategoryScale, LinearScale,
+//   BarElement, Title, Tooltip, Legend,)
 
 function App() {
+  // const [chartData, setChartData] = useState({
+  //   datasets: []
+  // })
+  // const [chartOptions, setChartOptions] = useState({})
+
+  const handleSubmit = () => {
+    papaParse(file_inc)
+
+  }
+
+  const papaParse = (inp_file) => {
+    let arrayObj = []
+    Papa.parse(inp_file.files[0],{
+      download: true,
+      header: true,
+      skipEmptyLines: true,
+      complete: ((results) => {
+        arrayObj = results.data
+        console.log(arrayObj)
+      })
+    })
+  }
 
   return (
     <div className="App">
-      <p className='text-9xl'>Hola</p>
+      <h1>Upload File</h1>
+      <div className='container'>
+        <input type="file" id='file_inc' accept='.csv'/>
+        <button className='btn' type='submit' onClick={handleSubmit}>Upload</button>
+      </div>
     </div>
   )
 }
